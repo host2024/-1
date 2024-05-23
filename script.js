58c9758375b3387b195a8cf671fc07d9d640f7d4
host2024 <youwoon5520@gmail.com>
2024-05-23 09:34:34 +0900

    과제

--- script.js
+++ script.js
@@ -1,149 +1,39 @@
-//  문제 1에서 만든 객체 배열
-
-// const all = [
-//     {
-//         이름: '철수',
-//         나이: 19,
-//         성별: '남',
-//     },
-//     {
-//         이름: '짱구',
-//         나이: 20,
-//         성별: '남',
-//     },
-//     {
-//         이름: '유리',
-//         나이: 21,
-//         성별: '여',
-//     },
-// ];
-
-// function displayData(all) {
-//     const outputDiv = document.getElementById('output');
-
-//     all.forEach((person) => {
-//         const personInfo = `
-//             <p>이름: ${person.이름}</p>
-//             <p>나이: ${person.나이}</p>
-//             <p>성별: ${person.성별}</p>
-//             <hr/>
-
-//         `;
-//         outputDiv.innerHTML += personInfo;
-//     });
-// }
-// displayData(all);
-
-// 문제 3
-// const all = [
-//     {
-//         이름: '철수',
-//         나이: 19,
-//         성별: '남',
-//     },
-//     {
-//         이름: '짱구',
-//         나이: 20,
-//         성별: '남',
-//     },
-//     {
-//         이름: '유리',
-//         나이: 21,
-//         성별: '여',
-//     },
-// ];
-
-// const female = all.find((person) => person.성별 === '여');
-
-// console.log(female);
-
-// 문제 4
-
-// const all = [
-//     {
-//         이름: '철수',
-//         나이: 19,
-//         성별: '남',
-//     },
-//     {
-//         이름: '짱구',
-//         나이: 20,
-//         성별: '남',
-//     },
-//     {
-//         이름: '유리',
-//         나이: 21,
-//         성별: '여',
-//     },
-// ];
-
-// const man = all.filter((person) => person.성별 === '남');
-
-// console.log(man);
-
-// 문제 5
-
-// const all = [
-//     {
-//         이름: '철수',
-//         나이: 19,
-//         성별: '남',
-//     },
-//     {
-//         이름: '짱구',
-//         나이: 20,
-//         성별: '남',
-//     },
-//     {
-//         이름: '유리',
-//         나이: 21,
-//         성별: '여',
-//     },
-// ];
-
-// const newAll = all.map((person) => ({
-//     ...person,
-//     나이: person.나이 + 10,
-// }));
-
-// console.log('기존 데이터:', all);
-// console.log('새로운 배열:', newAll);
-
-// 문제 6
-
-// const all = [
-//     {
-//         이름: '철수',
-//         나이: 19,
-//         성별: '남',
-//     },
-//     {
-//         이름: '짱구',
-//         나이: 20,
-//         성별: '남',
-//     },
-//     {
-//         이름: '유리',
-//         나이: 21,
-//         성별: '여',
-//     },
-// ];
-
-// const sortedAll = [...all].sort((a, b) => b.나이 - a.나이);
-
-// console.log('기존 데이터:', all);
-// console.log('정렬된 데이터:', sortedAll);
-
-// 문제 7
-
 // const user = {
 //     이름: '병수',
 //     나이: 20,
 //     주소: '시골',
 // };
 
-// const { 이름, 나이, 주소 } = user;
+// // 1. user 데이터를 로컬스토리지에 저장
+// localStorage.setItem('user', JSON.stringify(user));
 
-// console.log(이름);
-// console.log(나이);
-// console.log(주소);
+// // 2. 로컬 스토리지에 저장된 데이터 조회 및 콘솔 출력
+// const storedUser = localStorage.getItem('user');
+// const userObject = JSON.parse(storedUser);
+// console.log(userObject);
+
+// // 3. 나이를 30으로 변경 후 다시 저장
+// userObject.나이 = 30;
+// localStorage.setItem('user', JSON.stringify(userObject));
+
+// // 4. 로컬 스토리지의 데이터 삭제 및 확인
+// localStorage.removeItem('user');
+// const deletedUser = localStorage.getItem('user');
+// console.log(deletedUser); // null
+
+const apiURL = 'https://jsonplaceholder.typicode.com/posts';
+
+fetch(apiURL)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
